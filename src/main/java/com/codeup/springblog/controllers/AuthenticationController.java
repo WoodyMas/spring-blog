3086e1bf1458b93cd8fe98be9e44a209f3b689ab
Mason Wudtee <mason.wudtee@gmail.com>
2023-02-24 00:00:25 -0600

    Trying to make a 'forgot password'. so far I have a form that toggles visibility if link is clicked, but i might have to create a whole new view X(

--- src/main/java/com/codeup/springblog/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/springblog/controllers/AuthenticationController.java
@@ -1,12 +1,18 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.User;
+import com.codeup.springblog.repositories.UserRepository;
+import com.codeup.springblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthenticationController {
 
+    public EmailService emailService;
+    public UserRepository userDao;
 
 
     @GetMapping("/login")
@@ -15,7 +21,20 @@ public class AuthenticationController {
     }
 
     @PostMapping("/login")
-    public String loginSubmitted(){
+    public String loginSubmitted(@RequestParam (name = "username") String username, @RequestParam (name = "username-search") String usernameSearch){
+        if (usernameSearch != null) {
+
+
+            if (userDao.findByUsername(usernameSearch) != null) {
+                User requestingUser = userDao.findByUsername(usernameSearch);
+                System.out.println(requestingUser.getUsername());
+                emailService.prepareAndSend(requestingUser, String.format("Hi %s! You forgot your password!!", requestingUser.getUsername()), "Reset your password here: ");
+                return "/posts/splash";
+            }
+
+
+        }
+
         return "/posts/show";
     }
 
@@ -25,4 +44,8 @@ public class AuthenticationController {
     }
 
 
+    public static void main(String[] args) {
+
+        System.out.println();
+    }
 }
